a2b3a35077331d820aea091eb7595abd7c2a2035
Matthew Mitchell <matt.mancer@gmail.com>
2018-01-26 20:10:02 +0000

    Controls updated, alien can rotate

---
+++ Assets/Game/Scripts/AlienController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerController : MonoBehaviour {
+
+	public float speed;
+  public float rotationalSpeed;
+
+  Renderer renderer;
+
+  void Start() {
+    renderer = GetComponent<Renderer>();
+  }
+
+  void Update() {
+    float horiztontal = -Input.GetAxis("AlienHorizontal") * speed * Time.deltaTime;
+    float vertical = -Input.GetAxis("AlienVertical") * speed * Time.deltaTime;
+    float rotate = Input.GetAxis("AlienRotation") * rotationalSpeed * Time.deltaTime;
+
+    transform.Translate(new Vector3(horiztontal, vertical, 0f), Space.World);
+    transform.Rotate(rotate * Vector3.forward);
+
+    if (horiztontal != 0f || vertical != 0f) {
+      renderer.enabled = false;
+    } else {
+      renderer.enabled = true;
+    }
+  }
+}
--- Assets/Game/Scripts/EarthController.cs
+++ Assets/Game/Scripts/EarthController.cs
@@ -2,13 +2,13 @@
 
 public class DefenderController : MonoBehaviour {
 
-  public float speed = 90f;
+  public float speed;
 
   void Update() {
     transform.RotateAround(
       Vector3.zero,
       Vector3.forward,
-      -Input.GetAxis("Horizontal2") * speed * Time.deltaTime
+      -Input.GetAxis("EarthHorizontal") * speed * Time.deltaTime
     );
   }
 }

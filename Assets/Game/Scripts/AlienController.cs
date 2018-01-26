b9a160b41bfb01a19f5636bcfdb97fbf79e82673
Matthew Mitchell <matt.mancer@gmail.com>
2018-01-26 21:52:24 +0000

    Laser

--- Assets/Game/Scripts/AlienController.cs
+++ Assets/Game/Scripts/AlienController.cs
@@ -5,6 +5,12 @@ public class AlienController : MonoBehaviour {
 	public float speed;
   public float rotationalSpeed;
   public AlienSpawner spawner;
+  public GameObject rocketPrefab;
+  public GameObject radiowavePrefab;
+  public GameObject laserPrefab;
+  public Transform rocketSpawn;
+  public Transform radiowaveSpawn;
+  public Transform laserSpawn;
 
   Renderer renderer;
 
@@ -16,14 +22,25 @@ public class AlienController : MonoBehaviour {
     float horiztontal = -Input.GetAxis("AlienHorizontal") * speed * Time.deltaTime;
     float vertical = -Input.GetAxis("AlienVertical") * speed * Time.deltaTime;
     float rotate = Input.GetAxis("AlienRotation") * rotationalSpeed * Time.deltaTime;
+    bool fire = Input.GetButtonDown("AlienFire");
 
     transform.Translate(new Vector3(horiztontal, vertical, 0f), Space.World);
     transform.Rotate(rotate * Vector3.forward);
 
-    if (horiztontal != 0f || vertical != 0f) {
+    if ((horiztontal != 0f || vertical != 0f) && !fire) {
       renderer.enabled = false;
     } else {
       renderer.enabled = true;
+    }
+
+    if (fire) {
+      if (Physics.Raycast(rocketSpawn.position, rocketSpawn.right, Mathf.Infinity, LayerMask.GetMask("Planet"))) {
+        Debug.Log("Fire Rocket");
+      } else if (Physics.Raycast(radiowaveSpawn.position, radiowaveSpawn.right, Mathf.Infinity, LayerMask.GetMask("Planet"))) {
+        Debug.Log("Fire Radiowave");
+      } else if (Physics.Raycast(laserSpawn.position, laserSpawn.right, Mathf.Infinity, LayerMask.GetMask("Planet"))) {
+        Instantiate(laserPrefab, laserSpawn.position, laserSpawn.rotation);
+      }
     }
   }
 
---
+++ Assets/Game/Scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+  public float speed;
+
+  Transform origin;
+  Rigidbody rigidbody;
+
+  void Start() {
+    origin = transform;
+    rigidbody = GetComponent<Rigidbody>();
+  }
+
+  void Update() {
+    Vector3 transformation = transform.up * speed * Time.deltaTime;
+    rigidbody.MovePosition(transform.position + transform.TransformDirection(transformation));
+  }
+
+  void OnTriggetEnter(Collider other) {
+    if (other.tag == "Planet") {
+      speed = 0f;
+    }
+  }
+}

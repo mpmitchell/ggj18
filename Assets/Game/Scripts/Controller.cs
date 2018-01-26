060900c14e7c210a2b6aec368cbb5ccf22955c53
Matthew Mitchell <matt.mancer@gmail.com>
2018-01-26 18:35:18 +0000

    Basic Prototype

---
+++ Assets/Game/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+  public float speed = 90f;
+  public string inputAxis;
+
+  void Update() {
+    transform.RotateAround(
+      Vector3.zero,
+      Vector3.forward,
+      -Input.GetAxis(inputAxis) * speed * Time.deltaTime
+    );
+  }
+}

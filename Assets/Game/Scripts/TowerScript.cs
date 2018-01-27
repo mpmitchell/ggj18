c379de848101caf701f154c94b4f7cf1f8a52e03
winterfressh <bencekovacs00@gmail.com>
2018-01-27 01:54:27 +0000

    Added victory condition, tower damage

---
+++ Assets/Game/Scripts/TowerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerScript : MonoBehaviour {
+
+    public int health;
+    public Slider slider;
+    public float transmissionTimer;
+
+    void Start ()
+    {
+        health = 100;
+	}
+	
+	
+	void Update ()
+    {        
+        if (health >= 1)
+        {
+            transmissionTimer += Time.deltaTime;
+            if (transmissionTimer >= 5f)
+            {
+                slider.value++;
+                transmissionTimer = 0;
+            }
+
+        }
+        if (slider.value == 100)
+        {
+            Debug.Log("DEFENDING PLAYER WINS!");
+        }
+	}
+
+    public void LaserHit(int damagePerTick)
+    {
+        Debug.Log(this.health);
+        Debug.Log("HitByLaser");
+        health -= damagePerTick;
+    }
+
+    public void RocketHit(int damage)
+    {
+        Debug.Log(this.health);
+        Debug.Log("HitByRocket");
+        health -= damage;
+    }
+
+
+}

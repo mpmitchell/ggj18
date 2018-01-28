d64474bc0f04a4f355b44fcbaecfabde48a2c95c
Matthew Mitchell <matt.mancer@gmail.com>
2018-01-28 12:58:16 +0000

    Added Intro Screens

--- Assets/Game/Scripts/StartScreen.cs
+++ Assets/Game/Scripts/StartScreen.cs
@@ -1,11 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour {
 
+  public GameObject image1;
+  public GameObject image2;
+  public GameObject image3;
+
+  int stage = 0;
+
   void Update() {
     if (Input.anyKeyDown) {
-      SceneManager.LoadScene(2);
+      if (stage == 0) {
+        image1.SetActive(false);
+        image2.SetActive(true);
+        stage = 1;
+      } else if (stage == 1) {
+        image2.SetActive(false);
+        image3.SetActive(true);
+        stage = 2;
+      } else if (stage == 2) {
+        SceneManager.LoadScene(2);
+      }
     }
   }
 }
